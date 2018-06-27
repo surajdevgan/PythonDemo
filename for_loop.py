4a8c2e803fc581d2aab042825b05b98174f2c58b
surajdevgan <sdevgan007@gmail.com>
2018-06-27 09:25:44 +0530

    Base

---
+++ for_loop.py
@@ -0,0 +1,17 @@
+l=["sdsd",122,23.4,"asasa",323]
+for item in l:
+    print(item)
+
+Word = "Python"
+for letter in Word:
+    if(letter=='t'):
+        continue;
+    print(letter)
+
+print("New one")
+
+Words = "Suraj"
+for letter in Words:
+    if(letter=='a'):
+        break;
+    print(letter)
