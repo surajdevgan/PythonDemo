3613f164639bba1f5b218357f99f4815d30a3d1b
surajdevgan <sdevgan007@gmail.com>
2018-06-28 08:36:28 +0530

    Base

---
+++ Function.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+def main():
+    Display()
+    Result = Sum(12,10)
+    print(Result)
+
+def Display():
+    print("Hola Hola")
+    print("This is python funk")
+
+def Sum (n1,n2):
+    z = n1+n2
+    return z
+
+if __name__ == '__main__': main()
+
